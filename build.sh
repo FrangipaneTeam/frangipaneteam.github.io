3553899599b2055566e061aff03619efdd17aaf8
Ere Mid <eremid@gmail.com>
2023-03-23 15:43:16 +0100

    fix: add podman login

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 local_image="localhost/frangiweb:latest"
 registry_image="ghcr.io/frangipaneteam/frangiweb:latest"
 
-podman manifest rm $local_image
-podman manifest rm $registry_image
+podman manifest rm $local_image || true
+podman manifest rm $registry_image || true
 podman build --no-cache --jobs=4 --platform linux/arm64 --platform linux/amd64 --manifest $local_image .
 podman tag $local_image $registry_image
 podman manifest push --all $local_image docker://$registry_image
