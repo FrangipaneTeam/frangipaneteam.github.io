dd70c1548f805778467bb6fc47f093bc1858b2c7
Mickael Stanislas <contact@mickael-stanislas.com>
2023-04-26 10:06:31 +0200

    feat: add team

--- src/components/TeamMember/TeamMember.js
+++ src/components/TeamMember/TeamMember.js
@@ -1,27 +1,6 @@
-import { useContext } from 'react'
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import ButtonBase from '@mui/material/ButtonBase';
-import { ThemeContext } from '../../contexts/theme'
 import './TeamMember.css'
-
-const ImgRounded = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-  borderRadius: '50%',
-  border: '2px solid #ff00b7',
-  boxShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
-});
 
 const TeamMember = ({ member }) => (
   <div className='team_member'>
